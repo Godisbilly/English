90ca420614976dfdb9521d354623cb7a016aa3fe
GodisBilly <bewholehearted@icloud.com>
2024-05-26 23:57:40 +0800

    11

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,7 @@
 <template>
   <pageWrapper>
     <view>
+      <wd-calendar-view type="week" v-model="value" :first-day-of-week="1" @change="handleChange" />
       <view>
         <wd-cell v-for="cell in cells" :key="cell.title" :title="cell.title" :value="cell.value" />
       </view>
@@ -9,21 +10,25 @@
 </template>
 
 <script setup name="words">
-import pageWrapper from "/views/pageWrapper";
+import pageWrapper from "/views/pageWrapper"
 
-import {ref} from "vue";
+import { ref } from "vue"
+
+const value = ref(Date.now())
+
+function handleChange (val) { }
 const cells = ref([
   {
     title: "Words",
     label: "Label",
     value: "1",
   },
-    {
+  {
     title: "Sentenses",
     label: "Label",
     value: "2",
   },
-    {
+  {
     title: "Articles",
     label: "Label",
     value: "3",
