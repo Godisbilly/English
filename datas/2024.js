4e3acd529c6f708d1f71f350ffe67016714b6cdc
GodisBilly <bewholehearted@icloud.com>
2024-05-28 22:02:31 +0800

    11

---
+++ datas/2024.js
@@ -0,0 +1,57 @@
+const data = [
+  {
+    year: 2024,
+    section: 1,
+    articles: [
+      {
+        sentences: [
+          {
+            sentence:
+              "There’s nothing more welcoming than a door opening for you.",
+          },
+          {
+            sentence:
+              " $1$ the need to be touched to open or close, automatic doors are essential in $2$ disabled access to buildings and helping provide general $3$ to commercial buildings",
+          },
+          {
+            sentence:
+              "Self-sliding doors began to emerge as a commercial product in 1960 after being invented six years $4$ by Americans Dee Horton and Lew Hewitt.",
+          },
+        ],
+        answers: [],
+        topics: "",
+      },
+    ],
+  },
+  {
+    year: 2024,
+    section: 2,
+    part: A,
+    articles: [
+      {
+        sentences: [
+          {
+            sentence: "",
+            types: [],
+          },
+          {
+            sentence: "",
+            types: [],
+          },
+        ],
+        answers: [],
+        topics: "",
+      },
+      {},
+      {},
+      {},
+    ],
+  },
+  {
+    section: 2,
+    part: B,
+    articles: [],
+  },
+];
+
+export default data;
